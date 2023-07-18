d5b01295aa7ce5411660ec26627df82d40f3fedd
Mehdony <86969900+Mehdony@users.noreply.github.com>
2023-07-18 16:08:09 +0200

    Close button + style

--- Modal.jsx
+++ Modal.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-const Modal = ({ message, timeDuration }) => {
+const Modal = ({ message, setSent}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setIsOpen(true);
-    const timer = setTimeout(() => {
-      setIsOpen(false);
-    }, timeDuration);
-    return () => clearTimeout(timer); 
-  }, [timeDuration]);
+    // const timer = setTimeout(() => {
+    //   setIsOpen(false);
+    // }, timeDuration);
+    // return () => clearTimeout(timer); 
+  }, [message]);
+
+
+
 
   if (!isOpen) return null;
 
@@ -17,6 +20,7 @@ const Modal = ({ message, timeDuration }) => {
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#283728",  // Assuming you want the color blue
@@ -28,10 +32,26 @@ const Modal = ({ message, timeDuration }) => {
         color: "white",
         fontSize: "1.5rem",
         fontWeight: "bold",
+        rowGap: "1rem",
 
       }}
     >
       <div>{message}</div>
+      <button style={{
+        backgroundColor: "#000",  // Assuming you want the color blue
+        color: "white",
+        fontSize: "1.5rem",
+        fontWeight: "bold",
+        border: "none",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        cursor: "pointer",
+        width: "100px",
+        height: "50px",
+        textAlign: "center",
+        textDecoration: "none",
+        display: "inline-block"
+      }} onClick={() =>{setSent(false); setIsOpen(false)}}>Fermer</button>
     </div>
   );
 };
